fc5b9ca49aafbd4e0930a714463d9cdc33d5e774
ahmed4363 <ahmedbadeeb4363@gmail.com>
2026-03-03 01:27:55 +0800

    - Remove unnecesarry files

--- src/main/java/net/ahmed4363/decorativeladders/DecorativeLadders.java
+++ src/main/java/net/ahmed4363/decorativeladders/DecorativeLadders.java
@@ -3,12 +3,8 @@ package net.ahmed4363.decorativeladders;
 import net.ahmed4363.decorativeladders.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class DecorativeLadders implements ModInitializer {
 	public static final String MOD_ID = "decorative-ladders";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
